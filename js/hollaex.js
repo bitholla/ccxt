c4000edf2f7f3c94524fbadf64722eef584a7151
Brandon Kim <bkim2490@gmail.com>
2019-06-14 16:04:48 +0900

    fixed order for js

--- js/hollaex.js
+++ js/hollaex.js
@@ -15,7 +15,6 @@ module.exports = class hollaex extends Exchange {
             'countries': [ 'KR' ],
             'rateLimit': 333,
             'version': 'v0',
-            'verbose': true,
             'has': {
                 'CORS': false,
                 'fetchMarkets': true,
@@ -344,13 +343,12 @@ module.exports = class hollaex extends Exchange {
         let datetime = this.safeString (order, 'created_at');
         let timestamp = this.parse8601 (datetime);
         let lastTradeTimestamp = undefined;
-        let status = undefined;
+        let status = 'open';
         let type = this.safeString (order, 'type');
         let side = this.safeString (order, 'side');
         let price = this.safeFloat (order, 'price');
         let amount = this.safeFloat (order, 'size');
         let filled = this.safeFloat (order, 'filled');
-        // let remaining = amount - filled;
         let remaining = undefined;
         let cost = filled * price;
         let trades = undefined;
